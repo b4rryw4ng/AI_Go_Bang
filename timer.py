03946233331ba5ecbcfc3d57b2ac4d568ca9b149
Barry Wang <barrywang@MacBook-Pro-2.local>
2018-05-03 00:49:16 +0800

    temp

--- timer.py
+++ timer.py
@@ -3,9 +3,17 @@ import time
 
 start = time.time()
 print (start)
+a = 0
+b = 0
 while True :
     #end = time.time()
-
-    if time.time() - start > 4.8:
-        print (time.time() - start)
+    a += 1
+    if a == 10000000:
+        b+=1
+        a =0
+    if time.time() - start > 4.9:
+        
         break
+
+
+print (b,a)
